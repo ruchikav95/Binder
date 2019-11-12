31cb966bc5172a61ad1ff257f87341a96373b8ed
samanan.itthiphinyo@gmail.com <47881002+sitthiph@users.noreply.github.com>
2019-11-12 16:26:22 -0500

    MongoDB Structure included if switch

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ require("./routes/items-api-routes.js")(app);
 // });
 
 
+
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, () => {
