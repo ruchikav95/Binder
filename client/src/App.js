0e26e8834ad3ed0072da18dc5a253ad7b8fc0458
Ruchika Venkateswaran <rvss9591@gmail.com>
2019-11-12 01:43:33 -0500

    react-bootstrap and ant added, login form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-import AppHeader from './components/AppHeader';
-import AppContent from './components/AppContent';
+import Navbar from "./components/Navbar";
+import NormalLoginForm from "./components/Form";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <AppHeader />
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h2>Welcome to React</h2>
-        </div>
-        <AppContent />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <NormalLoginForm />
+      </React.Fragment>
     );
   }
 }
--- client/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {
-  Typography,
-} from '@material-ui/core';
-
-export default () => (
-  <Typography variant="h4">Welcome Home!</Typography>
-);
