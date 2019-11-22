98731518d9ee1b751bfbfd40e1dab5d43d4b72dc
Orlando Buzana <orlandobuzana@outlook.com>
2019-11-21 21:22:00 -0500

    Added list more component and reqwest module

--- frontend/src/view/home/HomePage.js
+++ frontend/src/view/home/HomePage.js
@@ -10,90 +10,24 @@ import HomeMixChartOne from 'view/home/HomeMixChartOne';
 import HomeHorizontalBarChart from 'view/home/HomeHorizontalBarChart';
 import HomePolarChart from 'view/home/HomePolarChart';
 import HomeLineChart from 'view/home/HomeLineChart';
-// import BookViewList from 'view/book/view/BookViewList';
+import LoadMoreList from 'view/book/view/LoadMoreList';
 // import ContentWrapper from 'view/layout/styles/ContentWrapper';
 // import BookView from 'view/book/view/BookView';
 class HomePage extends PureComponent {
   render() {
-    const twoColumnsResponsiveProps = {
-      xs: 24,
-      sm: 24,
-      md: 12,
-      lg: 12,
-      xl: 12,
-      style: { marginBottom: 24 },
-    };
 
-    const threeColumnsResponsiveProps = {
-      xs: 24,
-      sm: 24,
-      md: 12,
-      lg: 12,
-      xl: 8,
-      style: { marginBottom: 24 },
-    };
+
+   
+    
 
     return (
       
       <React.Fragment>
         
-         {/* <BookViewList /> */}
+         <LoadMoreList />
           
       
-        <Row gutter={24}>
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeDoughnutChart />
-            </Card>
-          </Col>
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeMixChartTwo />
-            </Card>
-          </Col>
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeBarChart />
-            </Card>
-          </Col>
-
-          <Col {...twoColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeMixChartOne />
-            </Card>
-          </Col>
-
-          <Col {...twoColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomePolarChart />
-            </Card>
-          </Col>
-
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeHorizontalBarChart />
-            </Card>
-          </Col>
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeLineChart />
-            </Card>
-          </Col>
-          <Col {...threeColumnsResponsiveProps}>
-            <Card bodyStyle={{ padding: 8 }}>
-              <HomeRadarChart />
-            </Card>
-          </Col>
-        </Row>
-        <p
-          style={{
-            width: '100%',
-            textAlign: 'center',
-            color: 'grey',
-          }}
-        >
-          {i18n('home.message')}
-        </p>
+        
       </React.Fragment>
     );
   }
